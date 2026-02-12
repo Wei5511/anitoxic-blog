a1712708496983f4a940f967aecfa9bf936255cd
Wei5511 <tzuyuandy@gmail.com>
2026-02-12 14:05:51 +0800

    Update favicon to book.png

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ export const metadata = {
     title: "【漫】性中毒 | Anime Addicts",
     description: "追蹤每季最新動漫，獲取即時資訊與精彩內容",
     icons: {
-        icon: '/book.jpg',
+        icon: '/book.png',
     },
     keywords: ["動漫", "anime", "新番", "季番", "日本動畫"],
     metadataBase: new URL('https://anitoxic-blog.vercel.app'),
